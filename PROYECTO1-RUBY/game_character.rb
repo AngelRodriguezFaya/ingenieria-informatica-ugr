b5eb8746c3b5e0b276ea3a51d21e996c2d984940
Ángel Rodríguez Faya <147878174+AngelRodriguezFaya@users.noreply.github.com>
2023-12-11 19:01:50 +0100

    Delete

--- PROYECTO1-RUBY/game_character.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-#encoding:utf-8
-
-module Irrgarten
-  module GameCharacter
-    PLAYER =:player
-    MONSTER =:monster
-  end   # end del módulo GameCharacter
-end  # end del módulo Irrgarten
--- PROYECTO1-RUBY/orientation.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-#encoding:utf-8
-
-module Irrgarten
-  module Orientation
-    VERTICAL =:vertical
-    HORIZONTAL =:horizontal
-  end   # end del módulo Orientation
-end  # end del módulo Irrgarten
